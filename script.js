5c943994ef635c8ce848ca26554f9dc5d6597e02
SriMoorthy <srimoorthy27@gmail.com>
2024-07-10 13:58:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+() => {
+    cy.visit(baseUrl + "/main.html"); 
+    //always check this "visit" in your test case that it is set to - baseUrl + "/main.html"
+    cy.get(".code-container").find("input.code").eq(5).type("{backspace}");
+    cy.focused().should("have.id", "code-6");
+    cy.get(".code").eq(4).type("{backspace}");
+    cy.focused().should("have.id", "code-5");
+    cy.get(".code").eq(3).type("{backspace}");
+    cy.focused().should("have.id", "code-4");
+    cy.get(".code").eq(2).type("{backspace}");
+    cy.focused().should("have.id", "code-3");
+    cy.get(".code").eq(1).type("{backspace}");
+    cy.focused().should("have.id", "code-2");
+    cy.get(".code").eq(0).type("{backspace}");
+    cy.focused().should("have.id", "code-1");
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     const inputs = document.querySelectorAll(".code");
     
