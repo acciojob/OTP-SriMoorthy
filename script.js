84ae6982c0470a5a1b867c976d4e3366c61832c2
SriMoorthy <srimoorthy27@gmail.com>
2024-07-31 20:47:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const codes = document.querySelectorAll('.code');
-
-codes.forEach((code, index) => {
-    code.addEventListener('input', (e) => {
-        const value = e.target.value;
-        if (value.length > 1) {
-            e.target.value = value[0];  // Take only the first character if more than one character is entered
+let codes = document.querySelectorAll(".code")
+// change, input
+codes.forEach((code,index)=>{
+    
+    code.addEventListener("input",()=>{
+        if(code.value.length == 1){
+             if(index < codes.length-1){
+                   codes[index+1].focus()
+             }
         }
-        if (value) {
-            if (index < codes.length - 1) {
-                codes[index + 1].focus();
-            }
+    })
+    // keyup, keydown(press)
+    code.addEventListener("keydown",(e)=>{
+           console.log(e.key)
+        if(e.key == "Backspace"){
+              if(index > 0 && code.value.length == 0){
+                codes[index-1].focus()
+              }
         }
-    });
-
-    code.addEventListener('keydown', (e) => {
-        if (e.key === 'Backspace' && !code.value && index > 0) {
-            codes[index - 1].focus();
-        }
-    });
-});
+    })
+})
