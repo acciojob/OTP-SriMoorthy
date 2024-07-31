f781a109fab6355e635a001d51c05f768d680fea
SriMoorthy <srimoorthy27@gmail.com>
2024-07-31 21:20:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -34,30 +34,3 @@ codes.forEach((code, index) => {
     });
 });
 
-cy.visit(baseUrl + "/main.html"); // Visit the page
-
-// Function to type into the field and check focus
-const typeAndCheckFocus = (index, value, expectedId) => {
-  cy.get(".code-container").find("input.code").eq(index).type(value);
-  cy.wait(100); // Add a small delay to ensure the focus has changed
-  cy.focused().should('have.attr', 'id', expectedId);
-};
-
-// Type into the first input field and check focus
-typeAndCheckFocus(0, '5', 'code-1');
-
-// Type into the second input field and check focus
-typeAndCheckFocus(1, '1', 'code-2');
-
-// Type into the third input field and check focus
-typeAndCheckFocus(2, '7', 'code-3');
-
-// Type into the fourth input field and check focus
-typeAndCheckFocus(3, '2', 'code-4');
-
-// Type into the fifth input field and check focus
-typeAndCheckFocus(4, '9', 'code-5');
-
-// Type into the sixth input field and check focus
-typeAndCheckFocus(5, '6', 'code-6');
-
